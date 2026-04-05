392658e30de81572876c717e6ed5b272df576bc2
applecaber29-cmyk <mondidooscar64@gmail.com>
2026-04-05 21:19:47 +0800

    FINISHING

--- src/Nav.js
+++ src/Nav.js
@@ -1,11 +1,9 @@
 import React, { useEffect, useState } from "react";
 import "./Nav.css";
 
-function Nav({ setSearchTerm }) {
+function Nav() {
   const [show, setShow] = useState(false);
-  const [menuOpen, setMenuOpen] = useState(false);
   const [profileOpen, setProfileOpen] = useState(false);
-  const [searchOpen, setSearchOpen] = useState(false);
 
   useEffect(() => {
     const handleScroll = () => {
@@ -17,52 +15,28 @@ function Nav({ setSearchTerm }) {
 
   return (
     <div className={`nav ${show && "nav__black"}`}>
-
-      {/* LEFT SIDE */}
+      
+      {/* LEFT SIDE - Logo */}
       <div className="nav__left">
         <img
           className="nav__logo"
-          src="https://res.cloudinary.com/dnsio9vml/image/upload/v1774948654/aa0ed93d-a2f9-4169-81b8-7621dfed8040-removebg-preview_eb13rq.png"
+          src="https://res.cloudinary.com/dnsio9vml/image/upload/v1775393715/c154ba48-c5e5-4f14-b4f8-559a668eb894-removebg-preview_wl7eup.png"
           alt="Netflix Logo"
         />
-
-        <ul className={`nav__menu ${menuOpen && "nav__menu--active"}`}>
-          <li>Home</li>
-          <li>TV Shows</li>
-          <li>Movies</li>
-          <li>New & Popular</li>
-          <li>My List</li>
-        </ul>
       </div>
 
-      {/* RIGHT SIDE */}
+      {/* RIGHT SIDE - Profile */}
       <div className="nav__right">
-
-        {/* Search Icon */}
-        <div className="nav__icon" onClick={() => setSearchOpen(!searchOpen)}>🔍︎</div>
-        {searchOpen && (
-          <input
-            type="text"
-            className="nav__search"
-            placeholder="Search movies..."
-            onChange={(e) => setSearchTerm(e.target.value)}
-            autoFocus
-          />
-        )}
-
-        {/* Bell Icon */}
-        <div className="nav__icon">🕭</div>
-
-        {/* Profile */}
         <div
           className="nav__profile"
           onClick={() => setProfileOpen(!profileOpen)}
         >
           <img
             className="nav__avatar"
-            src="https://res.cloudinary.com/dnsio9vml/image/upload/v1774974395/IMG_20250805_015209_157_lezviu.webp"
+            src="https://scontent.fmnl9-6.fna.fbcdn.net/v/t39.30808-6/635631975_1399514558535570_13704498839586752_n.jpg?_nc_cat=110&ccb=1-7&_nc_sid=1d70fc&_nc_eui2=AeGTa1gYRVMBcO_0SBOGCPgNJw_3OhQ8FXsnD_c6FDwVeymicOXeCLaK7YJ2ITY_CAjCZZa1dYVxILEgbxmSAy7_&_nc_ohc=ADFm8xIRx20Q7kNvwFfazEb&_nc_oc=AdpAf61BtYHGZPxeLXQ7R25L_twJSZA3wEmmIt2HTeR9ztN9JSGRt_Rw6hmI68dFvGI&_nc_zt=23&_nc_ht=scontent.fmnl9-6.fna&_nc_gid=cyQb9T3-vOebb8i08T_dNA&_nc_ss=7a3a8&oh=00_Af0HJK_DJM5IHJMXWJ4gkH8woDWoBOHFQTMoA9_8nAGRXQ&oe=69D84BD8"
             alt="User Avatar"
           />
+
           {profileOpen && (
             <div className="nav__dropdown">
               <p>Account</p>
@@ -71,15 +45,8 @@ function Nav({ setSearchTerm }) {
             </div>
           )}
         </div>
-
-        {/* Hamburger Menu */}
-        <div
-          className="nav__hamburger"
-          onClick={() => setMenuOpen(!menuOpen)}
-        >
-          ☰
-        </div>
       </div>
+
     </div>
   );
 }
