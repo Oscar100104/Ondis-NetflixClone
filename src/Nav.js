580e872111bb86664914238e9287544b50a37894
applecaber29-cmyk <mondidooscar64@gmail.com>
2026-04-07 17:46:27 +0800

    ItoNa

--- src/Nav.js
+++ src/Nav.js
@@ -1,32 +1,66 @@
 import React, { useEffect, useState } from "react";
 import "./Nav.css";
 
-function Nav() {
+function Nav({ setSearchTerm }) {
   const [show, setShow] = useState(false);
   const [profileOpen, setProfileOpen] = useState(false);
+  const [menuOpen, setMenuOpen] = useState(false);
+  const [searchOpen, setSearchOpen] = useState(false);
 
   useEffect(() => {
     const handleScroll = () => {
       setShow(window.scrollY > 100);
     };
+
     window.addEventListener("scroll", handleScroll);
     return () => window.removeEventListener("scroll", handleScroll);
   }, []);
 
   return (
     <div className={`nav ${show && "nav__black"}`}>
-      
-      {/* LEFT SIDE - Logo */}
+
+      {/* LEFT SIDE */}
       <div className="nav__left">
         <img
           className="nav__logo"
           src="https://res.cloudinary.com/dnsio9vml/image/upload/v1774948654/aa0ed93d-a2f9-4169-81b8-7621dfed8040-removebg-preview_eb13rq.png"
           alt="Netflix Logo"
         />
+
+        <ul className={`nav__menu ${menuOpen && "nav__menu--active"}`}>
+          <li>Home</li>
+          <li>TV Shows</li>
+          <li>Movies</li>
+          <li>New & Popular</li>
+          <li>My List</li>
+        </ul>
       </div>
 
-      {/* RIGHT SIDE - Profile */}
+      {/* RIGHT SIDE */}
       <div className="nav__right">
+
+        {/* SEARCH ICON */}
+        <span
+          className="nav__icon"
+          onClick={() => setSearchOpen(!searchOpen)}
+        >
+          🔍︎
+        </span>
+
+        {/* SEARCH INPUT */}
+        {searchOpen && (
+          <input
+            className="nav__search"
+            type="text"
+            placeholder="Search movies..."
+            onChange={(e) => setSearchTerm(e.target.value)}
+          />
+        )}
+
+        {/* BELL ICON */}
+        <span className="nav__icon">🔔</span>
+
+        {/* PROFILE */}
         <div
           className="nav__profile"
           onClick={() => setProfileOpen(!profileOpen)}
@@ -45,6 +79,15 @@ function Nav() {
             </div>
           )}
         </div>
+
+        {/* HAMBURGER MENU */}
+        <span
+          className="nav__hamburger"
+          onClick={() => setMenuOpen(!menuOpen)}
+        >
+          ☰
+        </span>
+
       </div>
 
     </div>
